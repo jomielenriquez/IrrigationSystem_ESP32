20f7668b6ef904c65791c2211b1190fa84c76def
Jomiel Enriquez <Jomiel.Enriquez@ph.gt.com>
2022-01-18 23:50:14 +0800

    File server included

--- ESP32.ino
+++ ESP32.ino
@@ -1,6 +1,14 @@
-
-//===========================sdcardModule
+#include <WiFi.h>              // Built-in
+#include <WiFiMulti.h>         // Built-in
+#include <ESP32WebServer.h>    // https://github.com/Pedroalbuquerque/ESP32WebServer download and place in your Libraries folder
+#include <ESPmDNS.h>
 #include "FS.h"
+
+#include "Network.h"
+#include "Sys_Variables.h"
+#include "CSS.h"
+//===========================sdcardModule
+//#include "FS.h"
 #include "SD.h"
 #include "SPI.h"
 //==========================ds3231
@@ -10,8 +18,8 @@
 #include "RTClib.h"
 
 //=================
-#include <WiFi.h>
-#include <PubSubClient.h>
+//#include <WiFi.h>
+//#include <PubSubClient.h>
 /*
  * Connect the SD card to the following pins:
  *
@@ -23,6 +31,9 @@
  * SCK      G18
  * CS       G5
  */
+
+WiFiMulti wifiMulti;
+ESP32WebServer server(80);
 
 //============================ds3231
 RTC_DS3231 rtc;
@@ -45,50 +56,10 @@ const char* mqtt_server = "broker.mqtt-dashboard.com";
 #define RXD2 16
 #define TXD2 17
 
-WiFiClient wifiClient;
+//WiFiClient wifiClient;
 
-PubSubClient client(wifiClient);
+//PubSubClient client(wifiClient);
 
-void setup_wifi() {
-    delay(10);
-    // We start by connecting to a WiFi network
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-    randomSeed(micros());
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    client.publish("outTopic", "hello world");
-    // Create a random client ID
-    String clientId = "ESP32Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str(),MQTT_USER,MQTT_PASSWORD)) {
-      Serial.println("connected");
-      //Once connected, publish an announcement...
-      client.publish("/ic/presence/esp32/", "hello world");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
 //===================SD Card Module
 
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
@@ -256,9 +227,25 @@ void setup() {
   
   Serial.begin(115200); //Serial must always be 115200 baud
   Serial.setTimeout(500);// Set time out for 
-  setup_wifi();
-  client.setServer(mqtt_server, mqtt_port);
-  reconnect();
+
+  if (!WiFi.config(local_IP, gateway, subnet, dns)) { //WiFi.config(ip, gateway, subnet, dns1, dns2);
+    Serial.println("WiFi STATION Failed to configure Correctly"); 
+  } 
+  wifiMulti.addAP(ssid_1, password_1);  // add Wi-Fi networks you want to connect to, it connects strongest to weakest
+  wifiMulti.addAP(ssid_2, password_2);  // Adjust the values in the Network tab
+  wifiMulti.addAP(ssid_3, password_3);
+  wifiMulti.addAP(ssid_4, password_4);  // You don't need 4 entries, this is for example!
+  
+  Serial.println("Connecting ...");
+  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect: scan for Wi-Fi networks, and connect to the strongest of the networks above
+    delay(250); Serial.print('.');
+  }
+  Serial.println("\nConnected to "+WiFi.SSID()+" Use IP address: "+WiFi.localIP().toString()); // Report which SSID and IP is in use
+  // The logical name http://fileserver.local will also access the device if you have 'Bonjour' running or your system supports multicast dns
+  if (!MDNS.begin(servername)) {          // Set your preferred server name, if you use "myserver" the address would be http://myserver.local/
+    Serial.println(F("Error setting up MDNS responder!")); 
+    ESP.restart(); 
+  } 
 
   // setup sd card module
   if (! rtc.begin()) {
@@ -270,7 +257,12 @@ void setup() {
   //Setup SD Card
   if(!SD.begin()){
         Serial.println("Card Mount Failed");
+        SD_present = false; 
         return;
+    }
+    else{
+      Serial.println(F("Card initialised... file access enabled..."));
+      SD_present = true; 
     }
     uint8_t cardType = SD.cardType();
 
@@ -292,16 +284,18 @@ void setup() {
 
     uint64_t cardSize = SD.cardSize() / (1024 * 1024);
     Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+    server.on("/",         HomePage);
+    server.on("/download", File_Download);
+    server.on("/delete", File_Delete);
+    ///////////////////////////// End of Request commands
+    server.begin();
+    Serial.println("HTTP server started");
 }
 
-void publishSerialData(char *serialData){
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.publish(MQTT_SERIAL_PUBLISH_CH, serialData);
-}
+
 void loop() {
-   client.loop();
+   server.handleClient(); // Listen for client connections
    
    DateTime now = rtc.now();
    String Time = String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second());
@@ -316,3 +310,101 @@ void loop() {
     appendFile(SD, "/SoilMoistureLog.txt", strToPrint.c_str());
    }
  }
+
+ void HomePage(){
+  SendHTML_Header();
+  webpage += F("<a href='/download'><button>Download</button></a>");
+  webpage += F("<a href='/delete'><button>Delete</button></a>");
+  append_page_footer();
+  SendHTML_Content();
+  SendHTML_Stop(); // Stop is needed because no content length was sent
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void File_Download(){ // This gets called twice, the first pass selects the input, the second pass then processes the command line arguments
+  //if (server.args() > 0 ) { // Arguments were received
+  //  if (server.hasArg("download")) 
+  SD_file_download("SoilMoistureLog.txt");
+  //}
+  //else SelectInput("File Download","Enter filename to download","download","download");
+}
+void File_Delete(){
+  deleteFile(SD, "/SoilMoistureLog.txt");
+  SendHTML_Header();
+  webpage += F("<a href='/download'><button>Download</button></a>");
+  webpage += F("<a href='/delete'><button>Delete</button></a>");
+  append_page_footer();
+  deletedMessage("Soil Moisture Log");
+  SendHTML_Content();
+  SendHTML_Stop(); // Stop is needed because no content length was sent
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void SD_file_download(String filename){
+  if (SD_present) { 
+    File download = SD.open("/"+filename);
+    if (download) {
+      server.sendHeader("Content-Type", "text/text");
+      server.sendHeader("Content-Disposition", "attachment; filename="+filename);
+      server.sendHeader("Connection", "close");
+      server.streamFile(download, "application/octet-stream");
+      download.close();
+    } else ReportFileNotPresent("download"); 
+  } else ReportSDNotPresent();
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void SendHTML_Header(){
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate"); 
+  server.sendHeader("Pragma", "no-cache"); 
+  server.sendHeader("Expires", "-1"); 
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN); 
+  server.send(200, "text/html", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves. 
+  append_page_header();
+  server.sendContent(webpage);
+  webpage = "";
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void SendHTML_Content(){
+  server.sendContent(webpage);
+  webpage = "";
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void SendHTML_Stop(){
+  server.sendContent("");
+  server.client().stop(); // Stop is needed because no content length was sent
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void SelectInput(String heading1, String heading2, String command, String arg_calling_name){
+  SendHTML_Header();
+  webpage += F("<h3 class='rcorners_m'>");
+  webpage += heading1+"</h3><br>";
+  webpage += F("<h3>"); 
+  webpage += heading2 + "</h3>"; 
+  webpage += F("<FORM action='/"); 
+  webpage += command + "' method='post'>"; // Must match the calling argument e.g. '/chart' calls '/chart' after selection but with arguments!
+  webpage += F("<input type='text' name='"); 
+  webpage += arg_calling_name; 
+  webpage += F("' value=''><br>");
+  webpage += F("<type='submit' name='"); 
+  webpage += arg_calling_name; 
+  webpage += F("' value=''><br><br>");
+  append_page_footer();
+  SendHTML_Content();
+  SendHTML_Stop();
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void ReportSDNotPresent(){
+  SendHTML_Header();
+  webpage += F("<h3>No SD Card present</h3>"); 
+  webpage += F("<a href='/'>[Back]</a><br><br>");
+  append_page_footer();
+  SendHTML_Content();
+  SendHTML_Stop();
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void ReportFileNotPresent(String target){
+  SendHTML_Header();
+  webpage += F("<h3>File does not exist</h3>"); 
+  webpage += F("<a href='/"); webpage += target + "'>[Back]</a><br><br>";
+  append_page_footer();
+  SendHTML_Content();
+  SendHTML_Stop();
+}
---
+++ Network.h
@@ -0,0 +1,18 @@
+// Adjust the following values to match your needs
+// -----------------------------------------------
+#define   servername "IS"     // Set your server's logical name here e.g. if myserver then address is http://myserver.local/
+IPAddress local_IP(192, 168, 254, 120); // Set your server's fixed IP address here
+IPAddress gateway(192, 168, 254, 254);    // Set your network Gateway usually your Router base address
+IPAddress subnet(255, 255, 255, 0);   // Set your network sub-network mask here
+IPAddress dns(192,168,254,254);           // Set your network DNS usually your Router base address
+const char ssid_1[]     = "PowerOfPhysics";
+const char password_1[] = "siJonasBakling";
+
+const char ssid_2[]     = "PowerOfPhysics_plus";
+const char password_2[] = "siJonasBakling";
+
+const char ssid_3[]     = "your_SSID3";
+const char password_3[] = "your_PASSWORD_for SSID3";
+
+const char ssid_4[]     = "your_SSID4";
+const char password_4[] = "your_PASSWORD_for SSID4";
